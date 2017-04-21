e390c45c4cc63ee6a59a2ab8a16490f4afdb85da
Lidan Liu <l.liu@ltwes37934.lan>
2017-04-21 15:36:07 +0100

    add keyword to api

--- middleware/pagination.js
+++ middleware/pagination.js
@@ -1,11 +1,12 @@
 function pagination (req, res, next){
   var offset = parseInt(req.query.offset || 0);
   var limit = parseInt(req.query.limit || 30);
+  var keyword = res.locals.keywordValue;
   res.locals.pagination = [];
   for (var i = 0; i < res.locals.pageNumber; i++) {
     res.locals.pagination[i] = {
         id: i+1,
-        url: "/api?offset="+(limit*i)+"&limit="+limit
+        url: "/api?offset="+(limit*i)+"&limit="+limit+"&keyword="+keyword
     }
 
   }
