c26cf338c2488f502ef3773381f1844d2d49d65f
sam <1352152608zyp@gmail.com>
2017-05-01 20:01:54 +0100

    add session

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var session = require('express-session');
+var bodyParser = require('body-parser')
 
 router.get('/', function(req, res){
   res.render('account', {
